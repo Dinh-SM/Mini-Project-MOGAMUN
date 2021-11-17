c74cdafaaa172d83660a28fff663b4b875685b7c
Dinh Son-Michel <smdinh@bs406-s34-14.insa-lyon.fr>
2021-11-17 12:00:33 +0100

    Beginning 2

--- mogamun.py
+++ mogamun.py
@@ -1,6 +1,10 @@
 # Imports
-import pandas as pd
+import glob
+import os.path
 import moganum_fun
+import numpy as np
+import pandas as pd
+from scipy.stats import norm
 
 # Initialize evolution parameters
 def moganum_init(
@@ -14,7 +18,7 @@ def moganum_init(
 		tournament_size = 2,
 		measure = "FDR",
 		threshold_deg = 0.05,
-		max_number_of_attemps = 3)
+		max_number_of_attemps = 3):
 
 	evolution_parameters = {
 		"generations" : generations,
@@ -39,19 +43,28 @@ def moganum_load_data(
 		differential_expression_path,
 		nodes_scores_path,
 		network_layers_dir,
-		layers)
+		layers):
 
 	measure = evolution_parameters["measure"]
 	threshold_deg = evolution_parameters["threshold_deg"]
 
-	de_results = pd.read_csv(differential_expression_path)
+	# columns : {gene, logFC, logCPM, PValue, FDR}
+	de_results = pd.read_csv(differential_expression_path, dtype = {"gene" : str, "logFC" : np.float64, "logCPM" : np.float64, "PValue": np.float64, "FDR" : np.float64})
 	de_results = remove_duplicates_de_results(de_results)
 
-	deg = de_results[de_results['FDR'] < threshold_deg]
+	deg = de_results[de_results["FDR"] < threshold_deg]
 
 	# verify existence of log(fold change) and consider it for the DEG
 	if "logFC" in deg.columns:
-		deg = deg[abs(deg['logFC']) > 1]
+		deg = deg[abs(deg["logFC"]) > 1]
 
-	# read the file names of the networks for the current experiment
-	
+	# read the file names of the networks for the current experiment, network_layers_dir needs to finish with '/'
+	files = glob.glob(network_layers_dir + "[" + layers + "]_*")
+
+	if len(files) < len(layers):
+		print("Error! One or more networks are missing from " + network_layers_dir)
+	else:
+		# if no nodes scores file exists
+        # calculate the nodes scores for all the genes in DE analysis results
+		if not os.path.exists(NodesScoresPath):
+			nodes_scores = 
--- mogamun_fun.py
+++ mogamun_fun.py
@@ -1,6 +1,6 @@
 # Filters the DE results, to remove duplicates
 def remove_duplicates_de_results(
-		de_results)
+		de_results):
 
 	# remove NAs
 	de_results = de_results.dropna(subset = ["gene"])
@@ -9,3 +9,29 @@ def remove_duplicates_de_results(
 	de_results_filtered = de_results.sort_values("FDR", ascending = True).drop_duplicates("gene").sort_index()
 
 	return de_results_filtered
+
+
+# Calculates the node score of a list of genes
+def get_nodes_scores_of_list_of_genes(
+		de_results,
+		list_of_genes,
+		measure):
+	
+	list_of_genes = list(set(list_of_genes))
+
+	# calculate nodes scores for the genes with formula: z = inverse CDF(1-p) 
+    # if gene has an expression value, and z = -Inf otherwise
+    nodes_scores
+    for gene in list_of_genes:
+    	if gene in de_results["gene"].tolist():
+    		de_results_gene = de_results[de_results["gene"] == gene]
+    		node_score = norm.ppf(1 - de_results_gene[measure])
+    	else:
+    		node_score = np.NINF
+    	nodes_scores.append(node_score)
+
+    # normalizes nodes scores to be in the range [0-1], ignoring +Inf and -Inf
+    for node_score in nodes_scores:
+    	min_nodes_scores = np.nanmin(nodes_scores[nodes_scores != np.NINF])
+    	max_nodes_scores = np.nanmax(nodes_scores[nodes_scores != np.INF])
+    	node_score = (node_score - min_nodes_scores) / (max_nodes_scores - min_nodes_scores)
